077a6c1b4c32af4aa771f9f74568a7aeb9d5778b
JoseXda <josegustavo6@protonmail.com>
2024-02-23 17:59:16 -0300

    devon: use aosp products

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_devon.mk
+    $(LOCAL_DIR)/aosp_devon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_devon-user \
-    lineage_devon-userdebug \
-    lineage_devon-eng
+    aosp_devon-user \
+    aosp_devon-userdebug \
+    aosp_devon-eng
