04e57fbae8d986935e02bb17a4b75766af026784
josexda <josegustavo6@protonmail.com>
2024-05-25 19:54:29 -0300

    devon: GenesisOS BringUP
    Change-Id: Ia0f93c9499bf205fdf681fa2c8f9256cce76ebde

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_devon.mk
+    $(LOCAL_DIR)/genesis_devon.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_devon-user \
-    aosp_devon-userdebug \
-    aosp_devon-eng
+    genesis_devon-ap1a-user \
+    genesis_devon-ap1a-userdebug \
+    genesis_devon-ap1a-eng
